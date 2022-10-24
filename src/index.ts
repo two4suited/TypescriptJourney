edfeb5d16464e56a2852fbaee89e96fb9bcd026d
Brian Sheridan <brian.sheridan@gmail.com>
2022-10-24 04:11:59 +0000

    Added launch.json and extensions to devcontainer

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export function add(num1: number, num2: number): number {
 export function multiply(num1: number,num2:number): number {
     return num1 * num2
 }
+
+let addedNum = add(1,2)
+console.log(addedNum)
