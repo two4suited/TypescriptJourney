1ba1ec56416ecd34cd0f4580b4b2001582130cb4
Brian Sheridan <brian.sheridan@gmail.com>
2022-11-05 18:13:43 -0700

    Cleanup

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export function add(num1: number, num2: number): number {
-    return num1+num2 
-}
-export function multiply(num1: number,num2:number): number {
-    return num1 * num2
-}
-
-let addedNum = add(1,2)
-console.log(addedNum)
